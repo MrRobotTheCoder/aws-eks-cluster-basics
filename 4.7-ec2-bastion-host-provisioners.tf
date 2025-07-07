e141d4b0a55315302bc3349691d38a143e5b86b9
Abinash Sethi <sethiabinash123@gmail.com>
2025-07-07 01:10:53 -0400

    corrected .pem file location

--- 4.7-ec2-bastion-host-provisioners.tf
+++ 4.7-ec2-bastion-host-provisioners.tf
@@ -13,12 +13,12 @@ resource "null_resource" "copy_ec2_keys" {
 ## File Provisioner: Copies the terraform-key.pem file to /tmp/terraform-key.pem
   provisioner "file" {
     source      = "private-key/aws-devops-key.pem"
-    destination = "/tmp/aws-devops-key.pem"
+    destination = "/home/ec2-user/aws-devops-key.pem"
   }
 ## Remote Exec Provisioner: Using remote-exec provisioner fix the private key permissions on Bastion Host
   provisioner "remote-exec" {
     inline = [
-      "sudo chmod 400 /tmp/aws-devops-key.pem"
+      "sudo chmod 400 /home/ec2-user/aws-devops-key.pem"
     ]
   }
 ## Local Exec Provisioner:  local-exec provisioner (Creation-Time Provisioner - Triggered during Create Resource)
